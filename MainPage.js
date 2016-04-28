79c6366f96809f75436a5ab89ab336aedf70ed48
chinmayik <chinmayi.kambhampati@gmail.com>
2016-04-28 10:08:31 -0700

    Added files via upload

---
+++ MainPage.js
@@ -0,0 +1,18 @@
+/**
+ * Created by vinodravipally on 4/27/16.
+ */
+myFunction()
+{
+
+ var x="you good";
+    var y="you bad";
+    if(document.getElementById('o1').checked)
+    {
+        window.alert(x);
+    }
+    else if(document.getElementById('o2').checked)
+    {
+        window.alert(y);
+    }
+
+}
