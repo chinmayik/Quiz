533d85d3f35b18f1adb5674fb8082267f60d2485
chinmayik <chinmayi.kambhampati@gmail.com>
2016-04-28 21:04:49 -0700

    Added files via upload

--- MainPage.js
+++ MainPage.js
@@ -1,18 +1,23 @@
 /**
  * Created by vinodravipally on 4/27/16.
  */
-myFunction()
+function myFun()
 {
-
- var x="you good";
-    var y="you bad";
-    if(document.getElementById('o1').checked)
+ var x="Vinnie you are the best best chubby bubbly hubby on the planet earth";
+    var y="Vinnie you so bad...you belong on Mars";
+    var z="Vinnie.. you messed up pretty bad!! Pick a side..be a better man!";
+    if((document.getElementById('o1').checked) && (document.getElementById('o3').checked))
     {
+
         window.alert(x);
     }
-    else if(document.getElementById('o2').checked)
+    else if((document.getElementById('o2').checked) && (document.getElementById('o4').checked))
     {
         window.alert(y);
+    }
+    else if(((document.getElementById('o1').checked) && (document.getElementById('o4')) || (document.getElementById('o2').checked) && (document.getElementById('o3').checked)))
+    {
+        window.alert(z);
     }
 
 }
