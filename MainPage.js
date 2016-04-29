77a435a0bacf3cae1fa9602378444395e90b7399
chinmayik <chinmayi.kambhampati@gmail.com>
2016-04-29 12:31:28 -0700

    Added files via upload

--- MainPage.js
+++ MainPage.js
@@ -6,6 +6,9 @@ function myFun()
  var x="Vinnie you are the best best chubby bubbly hubby on the planet earth";
     var y="Vinnie you so bad...you belong on Mars";
     var z="Vinnie.. you messed up pretty bad!! Pick a side..be a better man!";
+    var a="Select an option for the below questions";
+    var b="Select an option for question no.2";
+    var c="Select an option for question no.1";
     if((document.getElementById('o1').checked) && (document.getElementById('o3').checked))
     {
 
@@ -15,9 +18,22 @@ function myFun()
     {
         window.alert(y);
     }
-    else if(((document.getElementById('o1').checked) && (document.getElementById('o4')) || (document.getElementById('o2').checked) && (document.getElementById('o3').checked)))
+    else if(((document.getElementById('o1').checked) && (document.getElementById('o4').checked) || (document.getElementById('o2').checked) && (document.getElementById('o3').checked)))
     {
         window.alert(z);
     }
+    else if((!(document.getElementById('o1').checked) && !(document.getElementById('o2').checked)) && (!(document.getElementById('o3').checked) && !(document.getElementById('o4').checked)))
+    {
+        window.alert(a);
+    }
+    else if(((document.getElementById('o1').checked) || (document.getElementById('o2').checked)) && (!(document.getElementById('o3').checked) || !(document.getElementById('o4').checked)))
 
+    {
+        window.alert(b);
+    }
+    else if(((document.getElementById('o3').checked) || (document.getElementById('o4').checked)) && (!(document.getElementById('o1').checked) || !(document.getElementById('o2').checked)))
+
+    {
+        window.alert(c);
+    }
 }
